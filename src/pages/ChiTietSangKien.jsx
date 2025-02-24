048620869281146ed87b4239463571775aa0dcaf
phanxuanthang <phanxuanthang18111999@gmail.com>
2025-02-24 09:31:05 +0700

    update responssive

--- src/pages/ChiTietSangKien.jsx
+++ src/pages/ChiTietSangKien.jsx
@@ -7,18 +7,20 @@ export const ChiTietSangKien = () => {
     const params = useParams()
     const id = parseInt(params?.id)
     return (
-        <Container sx={{ marginTop: 8 }}>
+        <Container sx={{ marginTop: { xs: 4, md: 8 } }}>
             <PageTitleText>Sáng kiến số {id}</PageTitleText>
 
             {id === 1 &&
-                <Box>
-                    <Typography>Đọc nội dung</Typography>
-                    <div>
-                        <audio autoPlay controls>
-                            <source src="/sounds/sangkien.mp3" type="audio/mpeg" />
-                            Your browser does not support the audio element.
-                        </audio>
-                    </div>
+                <Box sx={{ marginTop: { xs: 1, md: 2 }, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <Box>
+                        <Typography sx={{ textAlign: { xs: 'center', md: 'center' } }}>Đọc nội dung</Typography>
+                        <div>
+                            <audio autoPlay controls>
+                                <source src="/sounds/sangkien.mp3" type="audio/mpeg" />
+                                Your browser does not support the audio element.
+                            </audio>
+                        </div>
+                    </Box>
                 </Box>
             }
 
--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -10,6 +10,7 @@ export const AppRoute = () => {
                 <Route path="sang-kien" element={<SangKien />} />
                 <Route path="sang-kien/:id" element={<ChiTietSangKien />} />
                 <Route path="chitietsangkien1.html" element={<ChiTietSangKien1ByHTML />} />
+                <Route path="chitietsangkien.html" element={<ChiTietSangKien1ByHTML />} />
                 <Route path="nha-truyen-thong" element={<NhaTruyenThong />} />
                 <Route path="loi-bac-ho-day" element={<LoiBacHoDay />} />
                 <Route path="loi-bac-ho-day/:id" element={<ChiTietLoiBacDay />} />
