0a3d4d93d9aee86ff2bbb53b7b703c5b56e29617
phanxuanthang <phanxuanthang18111999@gmail.com>
2025-02-24 07:12:40 +0700

    update

--- src/pages/ChiTietSangKien.jsx
+++ src/pages/ChiTietSangKien.jsx
@@ -1,6 +1,6 @@
 import { Box, Container, Typography } from "@mui/material"
 import { useParams } from "react-router"
-import { mockSangKien, PageTitleText } from "../constants"
+import { mockSangKien, mockSangKien2, PageTitleText } from "../constants"
 import { SunContent } from "../components"
 
 export const ChiTietSangKien = () => {
@@ -10,15 +10,17 @@ export const ChiTietSangKien = () => {
         <Container sx={{ marginTop: 8 }}>
             <PageTitleText>Sáng kiến số {id}</PageTitleText>
 
-            <Box>
-                <Typography>Đọc nội dung</Typography>
-                <div>
-                    <audio autoPlay controls>
-                        <source src="/sounds/sangkien.mp3" type="audio/mpeg" />
-                        Your browser does not support the audio element.
-                    </audio>
-                </div>
-            </Box>
+            {id === 1 &&
+                <Box>
+                    <Typography>Đọc nội dung</Typography>
+                    <div>
+                        <audio autoPlay controls>
+                            <source src="/sounds/sangkien.mp3" type="audio/mpeg" />
+                            Your browser does not support the audio element.
+                        </audio>
+                    </div>
+                </Box>
+            }
 
             {/* <Typography>Sang kien co id = {id}</Typography>
             <Typography>
@@ -26,7 +28,12 @@ export const ChiTietSangKien = () => {
                 Trải qua 79 năm xây dựng, chiến đấu, trưởng thành dưới sự lãnh đạo của Đảng, trực tiếp là Quân uỷ Trung ương - Bộ Quốc phòng, quân và dân Quân khu 3 đoàn kết một lòng, chủ động, sáng tạo hoàn thành xuất sắc mọi nhiệm vụ:
             </Typography> */}
 
-            <SunContent content={mockSangKien['editor-content']} />
+            {id === 1 &&
+                <SunContent content={mockSangKien['editor-content']} />
+            }
+            {id === 2 &&
+                <SunContent content={mockSangKien2['editor-content']} />
+            }
         </Container>
     )
 }
